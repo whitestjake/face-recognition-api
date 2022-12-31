fcfd1081b7697b2c1d593c96b990e9779698f79a
Jacob White <jakerobertwhite@gmail.com>
2022-12-30 20:55:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -50,9 +50,8 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     if (!email || !name || !password){
-        return res.status(400).json('incorrect form submission')
+        res.status(400).json('incorrect form submission')
     }
-    console.log('hello')
     const hash = bcrypt.hashSync(password)
     postgres.transaction(trx => {
         trx.insert({
