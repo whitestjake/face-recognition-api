4d76c56cf871dccf31f1b186ec8c364aac45c697
Jacob White <jakerobertwhite@gmail.com>
2023-01-01 22:24:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -43,8 +43,9 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
     console.log(name, email, password)
+    const { email, name, password } = req.body;
+    
     if (!email || !name || !password){
         return res.status(400).json(`incorrect form submission`)
     }
