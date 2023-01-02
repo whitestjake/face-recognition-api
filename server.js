5c142e10870101c3bbcfce1c66bcb708c8e54f26
Jacob White <jakerobertwhite@gmail.com>
2023-01-02 02:05:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -50,40 +50,29 @@ app.post('/register', (req, res) => {
     console.log(name, email, password)
     const hash = bcrypt.hashSync(password)
 
-    const loginQuery = {
-        hash: hash,
-        email: email
-    }
-
-    return postgres
-    .insert(loginQuery)
-    .into("login")
-    .then(res => console.log(res))
-    .catch(err => res.status(400).json("unable to insert into login"));
-
-    // postgres.transaction(trx => {
-    //     trx.insert({
-    //         hash: hash,
-    //         email: email
-    //     })
-    //     .into("login")
-    //     .returning("email")
-    //     .then(loginEmail => {
-    //         return trx("users")
-    //             .returning("*")
-    //             .insert({
-    //                 email: loginEmail[0].email,
-    //                 name: name,
-    //                 joined: new Date()
-    //             })
-    //             .then(user => {
-    //                 res.json(user[0]);
-    //             })
-    //             .then(trx.commit)
-    //             .catch(trx.rollback)
-    //     })
-    //     .catch( err => res.status(400).json("unable to register"))
-    // })  
+    postgres.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into("login")
+        .returning("email")
+        .then(loginEmail => {
+            return trx("users")
+                .returning("*")
+                .insert({
+                    email: loginEmail[0].email,
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+                .then(trx.commit)
+                .catch(trx.rollback)
+        })
+        .catch( err => res.status(400).json("unable to register"))
+    })  
 });
 
 app.get('/profile/:id', (req, res) => {
