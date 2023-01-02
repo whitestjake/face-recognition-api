607c965cbfb9ad2f537a3040473552084c3463e8
Jacob White <jakerobertwhite@gmail.com>
2023-01-02 01:47:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -50,29 +50,38 @@ app.post('/register', (req, res) => {
     console.log(name, email, password)
     const hash = bcrypt.hashSync(password)
 
-    postgres.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into("login").then(console.log('error here', trx))
-        .returning("email")
-        .then(loginEmail => {
-            return trx("users")
-                .returning("*")
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user[0]);
-                })
-                .then(trx.commit)
-                .catch(trx.rollback)
-        })
-        .catch( err => res.status(400).json("unable to register"))
-    })  
+    const loginQuery = {
+        hash: hash,
+        email: email
+    }
+
+    postgres
+    .query(loginQuery)
+    .catch(err => res.status(400).json("unable to insert into login"))
+
+    // postgres.transaction(trx => {
+    //     trx.insert({
+    //         hash: hash,
+    //         email: email
+    //     })
+    //     .into("login")
+    //     .returning("email")
+    //     .then(loginEmail => {
+    //         return trx("users")
+    //             .returning("*")
+    //             .insert({
+    //                 email: loginEmail[0].email,
+    //                 name: name,
+    //                 joined: new Date()
+    //             })
+    //             .then(user => {
+    //                 res.json(user[0]);
+    //             })
+    //             .then(trx.commit)
+    //             .catch(trx.rollback)
+    //     })
+    //     .catch( err => res.status(400).json("unable to register"))
+    // })  
 });
 
 app.get('/profile/:id', (req, res) => {
